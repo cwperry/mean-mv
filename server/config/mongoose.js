74452210a9d19031ee70e968e9280ac492e774f4
cwperry <chrisp@containerstore.com>
2014-05-01 06:29:54 -0500

    Finished first authentication module.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -11,15 +11,15 @@ module.exports = function(config) {
     var userSchema = mongoose.Schema({
         firstName: String,
         lastName: String,
-        userName: String
+        username: String
     });
     var User = mongoose.model('User', userSchema);
 
     User.find({}).exec(function(err, collection) {
         if(collection.length === 0) {
-            User.create({firstName:'Joe',lastName:'Eames',userName:'joe'});
-            User.create({firstName:'John',lastName:'Papa',userName:'john'});
-            User.create({firstName:'Dan',lastName:'Wahlin',userName:'dan'});
+            User.create({firstName:'Joe',lastName:'Eames',username:'joe'});
+            User.create({firstName:'John',lastName:'Papa',username:'john'});
+            User.create({firstName:'Dan',lastName:'Wahlin',username:'dan'});
         }
     });
 }
